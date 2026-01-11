23d01c23ff874f0de0a6af9e0bf98d0678af3d57
AhmedYeasin <ahmetchatgami@gmail.com>
2026-01-11 13:04:22 +0600

    Real date added from date-fns

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import logo from "../assets/logo.png"
+import React from "react";
+import logo from "../assets/logo.png";
+import { format } from "date-fns";
 const Header = () => {
-    return (
-        <div className='justify-center items-center flex-col flex'>
-            <img src={logo} alt="" />
-            <p className='text-accent'>Journalism With Fear & Dor</p>
-            
-        </div>
-    );
+  return (
+    <div className="justify-center items-center flex-col flex">
+      <img src={logo} alt="" />
+
+      <div className="py-3">
+        <p className="text-accent p-2">Journalism With Fear & Dor</p>
+
+        <p>{format(new Date(), "EEEE, MMMM MM, yyyy")}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
